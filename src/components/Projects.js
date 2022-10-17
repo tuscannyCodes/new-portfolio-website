71eb898bf696ed153ff6a90aa53d76887eb6368f
TuscannyCodes <tuscannypolk@gmail.com>
2022-10-17 16:33:11 -0400

    building projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,77 @@
+import { Container, Row, Col, Tab } from "react-bootstrap";
 export const Projects = () => {
-    const projects = [
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
 
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              lorem ipsum dolor sit am lorem ipsum dolor sit amet, con lorem
+              ipsum dolor sit amet
+            </p>
+            <Tab.Container id="projects-tabs">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab one</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="seconf">Tab two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <TabPane eventKey="first">
+<Row>
+    {projects.map((project,index)=>{
 
+        return(
+            <p>{project.title}</p>
+        )
+    })}
+</Row>
 
-    ];
-
-return(
-
-<>Projects</>
-
-    )
-}
+                </TabPane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
