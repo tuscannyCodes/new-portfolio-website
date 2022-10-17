e48198d8a54100fafa268716e828fa8417080931
TuscannyCodes <tuscannypolk@gmail.com>
2022-10-17 17:48:22 -0400

    building contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+
+export const Contact = () => {
+  const formInitialDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [butttonText, setButttonText] = useState("send");
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value,
+    });
+  };
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className=" align-items-center">
+          <Col md={6}>
+            <img src={contactImg} alt="Contact Us" />
+          </Col>
+          <Col md={6}>
+            <h2>Get In Touch</h2>
+            <form>
+              <Row>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    value={formDetails.firstName}
+                    placeholder="First Name"
+                    onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                  />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    value={formDetails.lastName}
+                    placeholder="Last Name"
+                    onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                  />
+                </Col>
+                /* continue css from tutorial @1:11:00 link:  https://www.youtube.com/watch?v=hYv6BM2fWd8&t=425s */
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
