bdac90328903a2ffd6966755eb587df0acc6dad4
TuscannyCodes <tuscannypolk@gmail.com>
2022-10-18 01:23:21 -0400

    minor updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/codeSnippet.png";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -66,7 +66,7 @@ export const Banner = () => {
             </button>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header Img" />
+            {/* <img src={headerImg} alt="Header Img" /> */}
           </Col>
         </Row>
       </Container>
